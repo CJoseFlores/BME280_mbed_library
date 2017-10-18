9ee13b41f43d4457de4294a5b66c0b57336a62d8
Carlos Flores <cflor083@fiu.edu>
2017-10-18 19:37:33 -0400

    Fixed specifier.

--- bme280_i2c.cpp
+++ bme280_i2c.cpp
@@ -6,7 +6,7 @@ BME280_I2C::BME280_I2C(PinName sda, scl, int frequency){
     //To be filled.
 }
 
-BME280_I2C::void reset(void){
+void BME280_I2C::reset(void){
         i2c.start();
         i2c.write(bmeAddr);
         i2c.write(0xB6); 
